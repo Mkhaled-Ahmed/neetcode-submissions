73de2343302ccb7dca6b5c3680e96eb9ab61ea37
Mohamed Khaled <142265860+Mkhaled-Ahmed@users.noreply.github.com>
2026-05-23 17:03:11 +0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        // const s_obj = {};
+        // const t_obj = {};
+        // if(s.length != t.length) return false;
+        // for(let  i = 0 ; i < s.length ; i ++){
+        //     s_obj[s[i]] = (s_obj[s[i]] || 0) + 1;
+        //     t_obj[t[i]] = (t_obj[t[i]] || 0) + 1;
+        // }
+        // for (let key in s_obj){
+        //     if(s_obj[key] != t_obj[key]) return false;
+        // }
+        // return true;
+
+        const sMap = new Map();
+        const tMap = new Map();
+        if(s.length != t.length) return false;
+        for(let i = 0 ; i < s.length ; i++){
+            sMap.set(s[i], (sMap.get(s[i]) || 0) + 1);
+            tMap.set(t[i], (tMap.get(t[i]) || 0) + 1);
+        }
+        for(let [key,value] of sMap) {
+            if(value != tMap.get(key) ){
+                return false;
+            }
+        }
+        return true;
+    }
+}
